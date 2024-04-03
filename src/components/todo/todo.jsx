dc5531454cf7496629f0cad40296566097dc9ea5
perpl777 <mllw327@gmail.com>
2024-04-03 12:31:50 +0700

    add change todo state

--- src/components/todo/todo.jsx
+++ src/components/todo/todo.jsx
@@ -1,28 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import styles from './todo.module.css';
 
 
 const Todo = () => {
-
     const [todos, setTodos] = useState([]);
     const [filter, setFilter] = useState('all');
+    // const [filteredTodos, setFilteredTodos] = useState([]);
+
+    const getTodos = async () => {
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=20");
+        const json = await response.json();
+        setTodos(json);
+    }
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=20");
-                const json = await response.json();
-                setTodos(json);
-                //console.log(todos)
-            } 
-            catch (error) {
-                console.error(error);
-            }
-        };
-
-        fetchData();
+        getTodos();
     }, []);
-
 
     const filterTodos = () => {
         if (filter === 'all') {
@@ -38,6 +31,45 @@ const Todo = () => {
 
     const filteredTodos = filterTodos();
 
+    const handleChangeState = (id) => {
+        const updatedTodos = todos.map(todo => {
+            if (todo.id === id) {
+                return {...todo, completed: !todo.completed};
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
+    }
+
+    // useEffect(() => {
+    //     setFilteredTodos(todos.filter((todo) => {
+    //         if (filter === 'all') {
+    //             return todos;
+    //         }
+    //         if (filter === 'completed') {
+    //             return todos.filter((todo) => todo.completed);
+    //         }
+    //         if (filter === 'uncompleted') {
+    //             return todos.filter((todo) => !todo.completed);
+    //         }
+    //     }));
+    // }, [todos, filter]);
+
+    // const filterTodos = useMemo(() => {
+    //     return todos.filter((todo) => {
+    //         if (filter === 'all') {
+    //             return todos;
+    //         }
+    //         if (filter === 'completed') {
+    //             return todos.filter((todo) => todo.completed);
+    //         }
+    //         if (filter === 'uncompleted') {
+    //             return todos.filter((todo) => !todo.completed);
+    //         }
+    //     })
+    // }, [todos]);
+
+
     return (
         <div className={styles.container}>
             <div className={styles.btns}>
@@ -47,9 +79,9 @@ const Todo = () => {
             </div>
 
             {filteredTodos && filteredTodos.map((todo) => (
-                <div key={todo.id}>
-                    <p className={todo.completed ? styles.completed : ''}>{todo.title}</p>
-                </div>
+                <button key={todo.id} onClick={() => handleChangeState(todo.id)} className={styles.todo}>
+                    <p className={todo.completed ? styles.completed : styles.uncompleted}>{todo.title}</p>
+                </button>
             ))
             }
         </div>
